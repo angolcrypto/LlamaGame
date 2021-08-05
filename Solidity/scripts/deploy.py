c1444be6e8502da6728db7c622bd1a558a06f79d
SAO <samuelangeloliver@gmail.com>
2021-08-05 19:56:16 +0100

    .

--- Solidity/scripts/deploy.py
+++ Solidity/scripts/deploy.py
@@ -2,4 +2,4 @@ from brownie import llama, accounts
 
 def main():
     acct = accounts.load('deployment_account')
-    llama.deploy({'from': acct})
+    llama.deploy({'from': acct}, publish_source=True)
